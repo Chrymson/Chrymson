61f692e79163b676bc311bb81a67d9f92c7c23c8
Chrymson <127989489+Chrymson@users.noreply.github.com>
2023-08-24 15:35:42 +0000

    0.0.1

---
+++ TicTacToe.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import random
+
+def next_turn():
+    pass
+
+def check_winner():
+    pass
+
+def empty_spaces():
+    pass
+
+def new_game():
+    pass
+
+window = Tk()
+window.title("Tik-Tak-Toe")
+players = ["x","o"]
+window.mainloop()
